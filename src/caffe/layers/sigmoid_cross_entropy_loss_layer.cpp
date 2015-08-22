d953601cb57e405ec2c3e8b6fce5cd5a79f50ff2
zengh123 <zengh@umich.edu>
2015-08-22 01:34:41 -0400

    finish debug for weighted loss

--- src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
+++ src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
@@ -18,7 +18,7 @@ void SigmoidCrossEntropyLossLayer<Dtype>::LayerSetUp(
   sigmoid_top_vec_.push_back(sigmoid_output_.get());
   sigmoid_layer_->SetUp(sigmoid_bottom_vec_, sigmoid_top_vec_);
     // Set Weight4pos
-  int USEWEIGHT = this->layer_param_.sig_param().use_weight();
+  bool USEWEIGHT = this->layer_param_.sig_param().use_weight();
   LOG(INFO) << "USEWEIGHT:" << USEWEIGHT;
 }
 
@@ -35,19 +35,51 @@ template <typename Dtype>
 void SigmoidCrossEntropyLossLayer<Dtype>::Forward_cpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) { 
   // The forward pass computes the sigmoid outputs.
+  bool USEWEIGHT = this->layer_param_.sig_param().use_weight();
   sigmoid_bottom_vec_[0] = bottom[0];
   sigmoid_layer_->Forward(sigmoid_bottom_vec_, sigmoid_top_vec_);
   // Compute the loss (negative log likelihood)
   const int count = bottom[0]->count();
   const int num = bottom[0]->num();
+  const int channels = bottom[0]->channels();
   // Stable version of loss computation from input data
   const Dtype* input_data = bottom[0]->cpu_data();
   const Dtype* target = bottom[1]->cpu_data();
   Dtype loss = 0;
-  for (int i = 0; i < count; ++i) {
+  Dtype pos_loss = 0;
+  Dtype neg_loss = 0;
+  Dtype p_avg_loss = 0;
+  Dtype n_avg_loss = 0;
+
+  int pos_targetnum = 0;
+  int neg_targetnum = 0;
+  // To achieve the same order of magnitude without using weight
+  // we mulitply it with channels firstly
+  if (USEWEIGHT == true){
+    for (int i = 0; i < count; ++i) {
+      if (target[i] == 1){
+        pos_targetnum++;
+        pos_loss -= input_data[i] * (target[i] - (input_data[i] >= 0)) -
+          log(1 + exp(input_data[i] - 2 * input_data[i] * (input_data[i] >= 0)));
+      }
+      else{        
+        neg_targetnum++;
+        neg_loss -= input_data[i] * (target[i] - (input_data[i] >= 0)) -
+          log(1 + exp(input_data[i] - 2 * input_data[i] * (input_data[i] >= 0)));
+      }
+    }
+
+    p_avg_loss = pos_targetnum>0? pos_loss / pos_targetnum : 0;
+    n_avg_loss = neg_targetnum>0? neg_loss / neg_targetnum : 0;
+    loss = 0.5 * (p_avg_loss + n_avg_loss) * channels;
+  }
+  else{
+    for (int i = 0; i < count; ++i) {
     loss -= input_data[i] * (target[i] - (input_data[i] >= 0)) -
         log(1 + exp(input_data[i] - 2 * input_data[i] * (input_data[i] >= 0)));
+    }
   }
+
   top[0]->mutable_cpu_data()[0] = loss / num;
 }
 
